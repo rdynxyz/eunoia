4cab6cdad2f6b197d891ce9ee99334324f3faf33
rdynxyz <radyan6661@gmail.com>
2022-10-07 15:44:54 +0700

    yuk gas

---
+++ apps.js
@@ -0,0 +1,9 @@
+let popUp = document.getElementById("popUpStartUp")
+function PUstartUp(){
+    // alert ("hi");
+    popUp.style.display = "block";
+}
+
+function closePU(){
+    popUp.style.display = "none";
+}
