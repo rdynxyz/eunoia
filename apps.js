56b9ad784535df2010109b90ca7779e2d0ef913f
rdynxyz <radyan6661@gmail.com>
2022-10-08 12:07:14 +0700

    gas

--- apps.js
+++ apps.js
@@ -1,21 +1,31 @@
-let PUstartup = document.getElementById("PUstartup")
-let PUbusiness = document.getElementById("PUbusiness")
-let PUbiz = document.getElementById("PUbiz")
+function navMenu(){
+    addEventListener.apply('click')
+}
 
+let PUstartup = document.getElementById("PUstartup");
 function CPstartup(){
     PUstartup.style.display = "block";
 } function closePU(){
     PUstartup.style.display = "none";
 }
 
+let PUbusiness = document.getElementById("PUbusiness");
 function CPbusiness(){
     PUbusiness.style.display = "block";
 } function closeBusiness(){
     PUbusiness.style.display = "none";
 }
 
+let PUbiz = document.getElementById("PUbiz");
 function CPbiz(){
     PUbiz.style.display = "block";
 } function closeBiz(){
     PUbiz.style.display = "none";
 }
+
+let PUenterprise = document.getElementById("PUenterprise")
+function CPenterprise(){
+    PUenterprise.style.display = "block";
+} function closeEnterprise(){
+    PUenterprise.style.display = "none";
+}   
