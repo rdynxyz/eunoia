3cae3c887cde43e8680d2660f81fb33255959588
azizfake <azizsigefake@gmail.com>
2022-10-09 09:35:47 +0700

    add: transition on navbar when opened

--- apps.js
+++ apps.js
@@ -1,32 +1,37 @@
-function navMenu(){
-    let navMenu = document.getElementById("nav-menu");
-    navMenu.style.right = "0px";
+function navMenu() {
+  let navMenu = document.getElementById("nav-menu");
+  navMenu.classList.toggle("right-[22rem]");
+  navMenu.classList.toggle("right-0");
 }
 
 let PUstartup = document.getElementById("PUstartup");
-function CPstartup(){
-    PUstartup.style.display = "block";
-} function closePU(){
-    PUstartup.style.display = "none";
+function CPstartup() {
+  PUstartup.style.display = "block";
+}
+function closePU() {
+  PUstartup.style.display = "none";
 }
 
 let PUbusiness = document.getElementById("PUbusiness");
-function CPbusiness(){
-    PUbusiness.style.display = "block";
-} function closeBusiness(){
-    PUbusiness.style.display = "none";
+function CPbusiness() {
+  PUbusiness.style.display = "block";
+}
+function closeBusiness() {
+  PUbusiness.style.display = "none";
 }
 
 let PUbiz = document.getElementById("PUbiz");
-function CPbiz(){
-    PUbiz.style.display = "block";
-} function closeBiz(){
-    PUbiz.style.display = "none";
+function CPbiz() {
+  PUbiz.style.display = "block";
+}
+function closeBiz() {
+  PUbiz.style.display = "none";
 }
 
-let PUenterprise = document.getElementById("PUenterprise")
-function CPenterprise(){
-    PUenterprise.style.display = "block";
-} function closeEnterprise(){
-    PUenterprise.style.display = "none";
-}   
+let PUenterprise = document.getElementById("PUenterprise");
+function CPenterprise() {
+  PUenterprise.style.display = "block";
+}
+function closeEnterprise() {
+  PUenterprise.style.display = "none";
+}
