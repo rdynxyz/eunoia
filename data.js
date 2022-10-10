a732e8f1cf9e4df6056d11fd9bf1349591decdfd
rdynxyz <radyan6661@gmail.com>
2022-10-10 15:38:03 +0700

    remove dark mode

--- data.js
+++ data.js
@@ -2,17 +2,23 @@
 function kirim(){
     var namaAwal = document.forms["contactForm"]["namaAwal"].value;
     var namaAkhir = document.forms["contactForm"]["namaAkhir"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var telp = document.forms["contactForm"]["telp"].value;
+    var subjek = document.forms["contactForm"]["subjek"].value;
+    
     let alpha = /^[a-zA-Z]+$/;
 
 
+    const name = localStorage.getItem("Name")
+
     if (namaAwal == "" || namaAkhir == ""){
-        alert ("name must be filled!");
+        alert ("Name must be filled!");
         return false;
     }
+
     if (namaAwal.match(alpha) && namaAkhir.match(alpha)){
         alert (`halo, ${namaAwal}`);
-    }
-    else{
-        alert ("Alpha only cuy!");
+    }   else{
+        alert ("Please enter alphabet in names form.");
     }
 }
